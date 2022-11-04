b6eb11a33f9e7a72051da8dd601b4e634e8d3a91
rnjsdbswo123 <64054467+rnjsdbswo123@users.noreply.github.com>
2022-11-04 18:12:44 +0900

    opacity slide 적용 중

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let findArrow = document.getElementById('arrowImg').className
 let finddreamStartDescriptionHead = document.querySelector('.dreamStartedDescription h2')
-
+let finddreamStartDescriptionP = document.querySelector('.dreamStartedDescription p')
+let findBannerTextHead = document.querySelector('.cleanJeju .bannerText h1')
+let findBannerTextP = document.querySelector('.cleanJeju .bannerText p')
+let findFirstClassEggHead = document.querySelector('.firstClassEgg h2')
+let findFirstClassEggP = document.querySelector('.firstClassEgg p')
 // arrow 이미지 깜빡거리기
 function arrowOpacity0(){
     if(findArrow ==='mobileBannerArrow'){
@@ -10,18 +14,61 @@ function arrowOpacity0(){
         findArrow = document.getElementById('arrowImg').className = 'mobileBannerArrow'
     }
 }
-// 나타나기
+
+
+// slide opacity 
 function finddreamStartDescriptionHeadSlide(){
     if(window.scrollY > 300){
-        console.log('하하하')
+        finddreamStartDescriptionHead.className = 'dreamStartedDescriptionH2Slide'
+        console.log(window.scrollY)
     }
-    
+}
+function finddreamStartDescriptionPSlide(){
+    if(window.scrollY > 450){
+        finddreamStartDescriptionP.className = 'dreamStartedDescriptionPSlide'
+        console.log(window.scrollY)
+    }
+}
+
+
+function finddreamBannerTextHeadSlide(){
+    if(window.scrollY > 900){
+        findBannerTextHead.className = 'bannerTexth1Slide'
+        console.log(window.scrollY)
+    }
+}
+function finddreamBannerTextPSlide(){
+    if(window.scrollY > 1100){
+        findBannerTextP.className = 'bannerTextPSlide'
+        console.log(window.scrollY)
+    }
+}
+
+
+function findFirstClassEggHeadSlide(){
+    if(window.scrollY > 1170){
+        findFirstClassEggHead.className = 'firstClassEggHead'
+        console.log(window.scrollY)
+    }
+}
+function findFirstClassEggPSlide(){
+    if(window.scrollY > 1350){
+        findFirstClassEggP.className = 'firstClassEggP'
+        console.log(window.scrollY)
+    }
 }
 
 
 
 
+
 document.addEventListener('scroll',finddreamStartDescriptionHeadSlide)
+document.addEventListener('scroll',finddreamStartDescriptionPSlide)
+document.addEventListener('scroll',finddreamBannerTextHeadSlide)
+document.addEventListener('scroll',finddreamBannerTextPSlide)
+document.addEventListener('scroll',findFirstClassEggHeadSlide)
+document.addEventListener('scroll',findFirstClassEggPSlide)
+
 
 setInterval(function()
 {
